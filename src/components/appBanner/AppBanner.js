76495bbda1b6528e5085b9ccf64ee3f3bf68e498
Artem <artempa1607@gmail.com>
2022-03-28 20:02:36 +0200

    loader added

--- src/components/appBanner/AppBanner.js
+++ src/components/appBanner/AppBanner.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
 import MarvelService from "../../services/MarvelService";
-import thor from "../../resources/img/Thumbnail.webp";
 
 import "./appBanner.scss";
 
@@ -10,53 +11,39 @@ class AppBanner extends Component {
     this.updateChar();
   }
   state = {
-    char: {
-      name: 'Thor',
-      description: null,
-      thumbnail: thor,
-      homepage: null,
-      wiki: null,
-    }
+    char: {},
+    loading: true,
+    error: false
   };
-
+  noDescritptionTxt = "There is no description for this character";
   marvelService = new MarvelService();
   onCharLoaded = (char) => {
-    this.setState({char});
-  }
+    this.setState({ char : {...char}, loading:false });
+  };
   updateChar = () => {
     const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-    this.marvelService.getCharacter(id).then(this.onCharLoaded);
+    this.marvelService.getCharacter(id).then(charObj => this.onCharLoaded(charObj)).catch(this.onError);
   };
 
+  onError = () =>{
+    this.setState({ loading:false, error: true });
+  }
+
   render() {
-    const { char: {name, description, thumbnail, homepage, wiki }} = this.state;
+    const {
+      char,
+      loading,
+      error
+    } = this.state;
+    const errorMessage = error ? <ErrorMessage/> : null;
+    const spinner = loading ? <Spinner/> : null;
+    const content = !(loading || error) ? <View char={char}/>: null;
     return (
       <div className="banner__container">
         <div className="banner__column">
-          <div className="banner__row random_char">
-            <div className="banner__img _img_random_char">
-              <img src={thumbnail} alt="CharacterImage" />
-            </div>
-            <div className="banner__info">
-              <div className="banner__info__title _title">{name}</div>
-              <div className="banner__info__subtitle _subtitle">
-                {description}
-                <br />
-                <br />
-              </div>
-              <div className="banner__info__buttonContainer _buttonContainer">
-                <a
-                  href={homepage}
-                  className="banner__button button__red _button"
-                >
-                  Homepage
-                </a>
-                <a href={wiki} className="banner__button button__gray _button">
-                  wiki
-                </a>
-              </div>
-            </div>
-          </div>
+          {errorMessage}
+          {spinner}
+          {content}
           <div className="banner__row tryIt_block">
             <div className="banner__info">
               <div className="banner__info__title _title">
@@ -86,5 +73,32 @@ class AppBanner extends Component {
     );
   }
 }
+
+const View = ({ char }) => {
+  const {name, description, thumbnail, homepage, wiki } = char;
+  return (
+    <div className="banner__row random_char">
+      <div className="banner__img _img_random_char">
+        <img src={thumbnail} alt="CharacterImage" />
+      </div>
+      <div className="banner__info">
+        <div className="banner__info__title _title">{name}</div>
+        <div className="banner__info__subtitle _subtitle">
+          {description}
+          <br />
+          <br />
+        </div>
+        <div className="banner__info__buttonContainer _buttonContainer">
+          <a href={homepage} className="banner__button button__red _button">
+            Homepage
+          </a>
+          <a href={wiki} className="banner__button button__gray _button">
+            wiki
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default AppBanner;
